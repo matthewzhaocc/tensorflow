5e594fdd77bd319952f120585af0ef3ad0bb1b2c
zhixianMatthewZhao <46915203+zhixianMatthewZhao@users.noreply.github.com>
2019-09-21 00:37:59 -0700

    Add files via upload

---
+++ textprocessing.py
@@ -0,0 +1,63 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+
+(train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=3000)
+
+word_index = keras.datasets.imdb.get_word_index()
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"],padding='post',maxlen=256)
+
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value = word_index["<PAD>"], padding='post', maxlen=256)
+
+data_size = 20000
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+model = keras.Sequential()
+model.add(keras.layers.Embedding(data_size,16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation=tf.nn.relu))
+model.add(keras.layers.Dense(1,activation=tf.nn.sigmoid))
+
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['acc'])
+
+x_val = train_data[:20000]
+partial_x_train = train_data[20000:]
+
+y_val = train_labels[:20000]
+partial_y_train = train_labels[20000:]
+
+history = model.fit(partial_x_train,partial_y_train,epochs=40,batch_size=512,validation_data=(x_val, y_val), verbose=1)
+
+history_dict = history.history
+
+import matplotlib.pyplot as plt
+
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
